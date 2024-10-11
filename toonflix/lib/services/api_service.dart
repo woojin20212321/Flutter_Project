e115a7ea56326d7b6c222e7eeb45c8a364a9afb9
woojin <gksdnwls391@naver.com>
2024-10-11 23:57:44 +0900

    fromJson

--- toonflix/lib/services/api_service.dart
+++ toonflix/lib/services/api_service.dart
@@ -4,8 +4,9 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
   Future<List<WebtoonModel>> getTodayToons() async {
     List<WebtoonModel> webtoonInstances = [];
