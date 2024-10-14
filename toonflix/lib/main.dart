b9c55b793bd1493703488435b30cb41c980b259b
woojin <gksdnwls391@naver.com>
2024-10-14 20:08:20 +0900

    Hero

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
-import 'package:toonflix/services/api_service.dart';
 
 void main() {
   runApp(const MyApp());
